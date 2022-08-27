2960a8ab1dbbf5930430c16ad968b8e7c07b9e94
xuri02 <t0bl4s.durst@gmail.com>
2022-08-27 22:47:15 +0200

    Made logger require init

--- src/esp32/include/log.hpp
+++ src/esp32/include/log.hpp
@@ -2,42 +2,55 @@
 
 #include <Arduino.h>
 
-class Logger : public Stream
-{
-    String* _data;
-
+class Logger : public Stream {
+    String *_data;
+    bool init = false;
 public:
-    Logger(){
+    bool Init(int baud = 115200) {
+        if (init) return true;
+        Serial.begin(baud);
+        init = true;
+        println("[LOGGER] Started");
+        return true;
+    }
+
+    Logger() {
         _data = new String();
     }
-    ~Logger(){
+
+    ~Logger() {
         delete _data;
     }
-    size_t write(uint8_t c) override
-    {
-        _data->concat((char)c);
+
+    size_t write(uint8_t c) override {
+        if (!Init())return 0;
+        _data->concat((char) c);
         return 1;
     }
-    size_t write(const uint8_t *buffer, size_t size) override
-    {
+
+    size_t write(const uint8_t *buffer, size_t size) override {
+        if(!Init())return 0;
         _data->concat(buffer, size);
         return Serial.write(buffer, size);
     }
-    String Data()
-    {
+
+    String Data() {
+        if(!Init())return "";
         return *_data;
     }
 
-    int available() override
-    {
+    int available() override {
+        if(!Init())return 0;
         return Serial.available();
     }
-    int read() override
-    {
+
+    int read() override {
+        if(!Init())return 0;
         return Serial.read();
     }
-    int peek() override
-    {
+
+    int peek() override {
+        if(!Init())return 0;
         return Serial.peek();
     }
 } logger;
